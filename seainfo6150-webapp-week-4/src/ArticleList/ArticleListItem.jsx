2f1bdc659f567083fce1f7c3963bf98de0082847
TianYin-y <55065711+TianYin-y@users.noreply.github.com>
2019-10-08 20:11:13 -0700

    Update ArticleListItem.jsx

--- seainfo6150-webapp-week-4/src/ArticleList/ArticleListItem.jsx
+++ seainfo6150-webapp-week-4/src/ArticleList/ArticleListItem.jsx
@@ -3,7 +3,9 @@ import React from "react"
 const articlelistitem = ({article}) => {
     return(
         <div>
-            <p><time dateTime={article.pubDate}>{article.pubDate}</time><button onclick={() => alert(`${article.slug}`)}>show article slug</button></p>
+            <p><time dateTime={article.pubDate}>{article.pubDate}</time> 
+            <button onClick={() => {alert(`${article.slug}`)}}>show article slug</button>
+            </p>
 
             </div>
             
